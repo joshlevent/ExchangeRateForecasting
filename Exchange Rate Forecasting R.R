ac7e3c50b0fbe2bd06e4b5b4b45f6e56da5f591e
beaudj <43608391+beaudj@users.noreply.github.com>
2023-11-12 23:30:02 +0100

    Add files via upload

--- Exchange Rate Forecasting R.R
+++ Exchange Rate Forecasting R.R
@@ -6,17 +6,26 @@
 # ------------------------------------------------------------------------------
 
 # ------------------------------------------------------------------------------
-# 0) Packages and functions that we need
+# Packages and functions that we need
 # ------------------------------------------------------------------------------
 library(tsbox)
 library(forecast)
 library(xts)
+library(tsbox)
 library(ggplot2)   
 library(reshape2)
 library(dataseries)
 library(seasonal)
 library(readxl) 
 library(XML)
+library(CADFtest)
+library(quantmod)
+library(gridExtra)
+library(forecast)
+
+install.packages("DataEditR")
+library(DataEditR)
+
 
 # Delete all objects in the memory
 rm(list=ls())
@@ -30,10 +39,22 @@ outDir <- makeOutDir(mainDir, "/ResultsApp6")
 
 
 # ------------------------------------------------------------------------------
-# 1) Import Data
+# Import and clean Data
 # ------------------------------------------------------------------------------
+
+Data <- read.csv("https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1319&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=SOFR&scale=left&cosd=2018-04-03&coed=2024-11-09&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Daily&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date=2024-11-12&revision_date=2024-11-12&nd=2018-04-03")
+
+SOFR   = xts(Data[,2], order.by = as.Date(paste(Data[,1],"-01", sep = "")))
+
+Data <- read.csv("https://www.federalreserve.gov/datadownload/Output.aspx?rel=H10&series=f838388dca2fd4e8bdfb846f3d2c35df&lastobs=&from=01/01/1971&to=10/09/2024&filetype=csv&label=include&layout=seriescolumn")
+
+Data = Data[-1,]
+Data = Data[-1,]
+Data = Data[-1,]
+Data = Data[-1,]
+Data = Data[-1,]
+
+SWISSDOLLAR  = xts(Data[,2], order.by = as.Date(paste(Data[,1],"-01", sep = "")))
+
 Saron <- read.csv("https://www.six-group.com/exchanges/downloads/indexdata/hsrron.csv")
 
-SOFR <- read.csv("https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo=open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=on&txtcolor=%23444444&ts=12&tts=12&width=1319&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_tooltip=yes&id=SOFR&scale=left&cosd=2018-04-03&coed=2024-11-09&line_color=%234572a7&link_values=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Daily&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin&vintage_date=2024-11-12&revision_date=2024-11-12&nd=2018-04-03")
-
-SWISSDOLLAR <- read.csv("https://www.federalreserve.gov/datadownload/Output.aspx?rel=H10&series=f838388dca2fd4e8bdfb846f3d2c35df&lastobs=&from=01/01/1971&to=10/09/2024&filetype=csv&label=include&layout=seriescolumn")
